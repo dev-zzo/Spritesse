cdfc65d885e1d3e097f4b01698ba6f62b7e0c09c
dev-zzo <gauri@tut.by>
2016-04-24 00:47:00 +0200

    tiled: lost file

---
+++ ThreeSheeps.Tiled.PipelineExts/TxxLayerContent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ThreeSheeps.Tiled
+{
+    /// <summary>
+    /// Base class for layered objects.
+    /// </summary>
+    public class TxxLayerContent
+    {
+        /// <summary>
+        /// Object group name.
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// Rendering offset for this object group, in pixels.
+        /// </summary>
+        public Point RenderingOffset;
+        /// <summary>
+        // Whether the objects in this group are visible.
+        /// </summary>
+        public bool Visible;
+        /// <summary>
+        /// How transparent is this object group.
+        /// </summary>
+        public float Opacity;
+        /// <summary>
+        /// Contains any custom properties set for this node.
+        /// </summary>
+        public TxxProperties Properties;
+    }
+}
