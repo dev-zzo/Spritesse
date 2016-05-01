baaf4259181adc4bf655bf24fff2e15f7b777428
dev-zzo <gauri@tut.by>
2016-05-01 15:12:30 +0200

    add (another) math helper class

---
+++ ThreeSheeps.Spritesse/MathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ThreeSheeps.Spritesse
+{
+    public static class MathHelper
+    {
+        public static uint RountToPowerOfTwo(uint x)
+        {
+            if (x == 0)
+                return 1;
+
+            x--;
+            x |= x >> 1;
+            x |= x >> 2;
+            x |= x >> 4;
+            x |= x >> 8;
+            x |= x >> 16;
+            return x + 1;
+        }
+    }
+}
