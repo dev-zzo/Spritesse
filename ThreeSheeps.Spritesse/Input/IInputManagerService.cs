76e9dfe637cb278362b78d56f8d7593b4605a97f
dev-zzo <gauri@tut.by>
2016-05-20 19:17:43 +0200

    issue #1: implement a very simple generic input handler

---
+++ ThreeSheeps.Spritesse/Input/IInputManagerService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace ThreeSheeps.Spritesse.Input
+{
+    /// <summary>
+    /// Represents a more functional in-game input handler.
+    /// </summary>
+    /// <typeparam name="TAction">An enum type representing bindable actions.</typeparam>
+    public interface IInputManagerService<TAction>
+    {
+        /// <summary>
+        /// Bind an action to a specific key.
+        /// </summary>
+        /// <param name="action">Action to be bound to</param>
+        /// <param name="key">Key to be bound</param>
+        void SetKeyBinding(TAction action, Keys key);
+        /// <summary>
+        /// Query the currently bound key for an action.
+        /// </summary>
+        /// <param name="action">Action to query for</param>
+        /// <returns>The key if bound, null otherwise</returns>
+        Keys? GetKeyBinding(TAction action);
+
+        /// <summary>
+        /// Query whether an action has been activated (e.g. the key has just been pressed).
+        /// This is an "edge triggered" event.
+        /// </summary>
+        /// <param name="action">Action to query for</param>
+        bool HasBeenActivated(TAction action);
+
+        /// <summary>
+        /// Query whether an action has been deactivated (e.g. the key has just been released).
+        /// This is an "edge triggered" event.
+        /// </summary>
+        /// <param name="action">Action to query for</param>
+        bool HasBeenDeactivated(TAction action);
+
+        /// <summary>
+        /// Query whether an action is active (the key is pressed down).
+        /// This is a "level triggered" event.
+        /// </summary>
+        /// <param name="action">Action to query for</param>
+        /// <returns>True if the action is currently active, false otherwise (also if not bound to any key)</returns>
+        bool IsActive(TAction action);
+    }
+}
---
+++ ThreeSheeps.Spritesse/Input/InputManagerComponent.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ThreeSheeps.Spritesse.Input
+{
+    public class InputManagerComponent<TAction> : GameComponent, IInputManagerService<TAction>
+    {
+        public InputManagerComponent(Game game)
+            : base(game)
+        {
+            game.Services.AddService(typeof(IInputManagerService<TAction>), this);
+        }
+
+        public void SetKeyBinding(TAction action, Keys key)
+        {
+            this.keyBindings[action] = key;
+        }
+
+        public Keys? GetKeyBinding(TAction action)
+        {
+            Keys key;
+            if (this.keyBindings.TryGetValue(action, out key))
+                return key;
+            return null;
+        }
+
+        public bool HasBeenActivated(TAction action)
+        {
+            Keys key;
+            if (this.keyBindings.TryGetValue(action, out key))
+            {
+                return this.currentKeyboardState[key] == KeyState.Down && this.previousKeyboardState[key] == KeyState.Up;
+            }
+            return false;
+        }
+
+        public bool IsActive(TAction action)
+        {
+            Keys key;
+            if (this.keyBindings.TryGetValue(action, out key))
+            {
+                return this.currentKeyboardState[key] == KeyState.Down;
+            }
+            return false;
+        }
+
+        public bool HasBeenDeactivated(TAction action)
+        {
+            Keys key;
+            if (this.keyBindings.TryGetValue(action, out key))
+            {
+                return this.currentKeyboardState[key] == KeyState.Up && this.previousKeyboardState[key] == KeyState.Down;
+            }
+            return false;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            this.previousKeyboardState = this.currentKeyboardState;
+            this.currentKeyboardState = Keyboard.GetState();
+        }
+
+        private KeyboardState currentKeyboardState;
+        private KeyboardState previousKeyboardState;
+        private readonly Dictionary<TAction, Keys> keyBindings = new Dictionary<TAction, Keys>();
+    }
+}
